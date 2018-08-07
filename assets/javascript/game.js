590369b847a678968d99044bd213e860c268ca0e
Bee <bee406@users.noreply.github.com>
2018-08-06 23:12:15 -0600

    css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var targetNumber = 0;
 
 //create crystalNumber variable 
-var crystalNumber = [Math.floor(Math.random() * (15 - 2) + 2), Math.floor(Math.random() * (15 - 2) + 2), Math.floor(Math.random() * (15 - 2) + 2), Math.floor(Math.random() * (15 - 2) + 2)];
+var crystalNumber = [Math.floor(Math.random() * (12 - 1) + 2), Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1)];
 
 var images = ["assets/images/crystal1.jpg", "assets/images/crystal2.jpg", "assets/images/crystal3.jpg", "assets/images/crystal4.jpg"];
 
@@ -22,7 +22,7 @@ var losses = 0;
 
 function resetGame() {
     //reset target number
-    targetNumber = Math.floor(Math.random() * (100 - 30) + 30);
+    targetNumber = Math.floor(Math.random() * (120 - 19) + 19);
     $("#target-number").html(targetNumber);
 
     //resetting user's score
@@ -30,7 +30,7 @@ function resetGame() {
     $("#total-score").html(totalScore);
 
     // reset crystal values
-    crystalNumber = [Math.floor(Math.random() * (15 - 2) + 2), Math.floor(Math.random() * (15 - 2) + 2), Math.floor(Math.random() * (15 - 2) + 2), Math.floor(Math.random() * (15 - 2) + 2)];
+    crystalNumber = [Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1)];
     for (var i = 0; i < crystalNumber.length; i++) {
         crystalImage.attr("data-crystalvalue", crystalNumber[i]);
     };
@@ -59,15 +59,3 @@ $(".crystal-image").on("click", function () {
     }
 
 });
-
-
-
-
-
-//update the player's score with number from crystal
-
-//if total score equals random number, the player wins the game
-
-//if the total score is greater than random number, the player loses the game
-
-//the game restarts when the player wins or loses
