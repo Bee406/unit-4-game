3bc4e5cc317139f9f62f789d25cfe2cf846346d4
Bee <bee406@users.noreply.github.com>
2018-08-07 21:26:37 -0600

    style updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,14 @@
 var targetNumber = 0;
 
 //create crystalNumber variable 
-var crystalNumber = [Math.floor(Math.random() * (12 - 1) + 2), Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1)];
+var crystalNumber = [Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1)];
 
 var images = ["assets/images/crystal1.jpg", "assets/images/crystal2.jpg", "assets/images/crystal3.jpg", "assets/images/crystal4.jpg"];
 
+var crystalImage;
+//maybe make this a function we can call later
 for (var i = 0; i < crystalNumber.length; i++) {
-    var crystalImage = $("<img>");
+    crystalImage = $("<img>");
     crystalImage.addClass("crystal-image");
     crystalImage.attr("src", images[i]);
     crystalImage.attr("data-crystalvalue", crystalNumber[i]);
@@ -20,23 +22,29 @@ var wins = 0;
 
 var losses = 0;
 
+
 function resetGame() {
+
+    //resetting user's score
+    totalScore = 0
+    $("#total-score").html(totalScore);
+
     //reset target number
     targetNumber = Math.floor(Math.random() * (120 - 19) + 19);
     $("#target-number").html(targetNumber);
 
-    //resetting user's score
-    totalScore=0
-    $("#total-score").html(totalScore);
-
-    // reset crystal values
     crystalNumber = [Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1), Math.floor(Math.random() * (12 - 1) + 1)];
-    for (var i = 0; i < crystalNumber.length; i++) {
+    console.log(crystalNumber);
+    for (var i = 0; i < crystalNumber.length; i++) {    
         crystalImage.attr("data-crystalvalue", crystalNumber[i]);
-    };
+
+    }
+
 };
 
+
 resetGame();
+
 
 $(".crystal-image").on("click", function () {
     var crystalValue = ($(this).attr("data-crystalvalue"));
@@ -47,15 +55,17 @@ $(".crystal-image").on("click", function () {
     if (totalScore === targetNumber) {
         wins++;
         $("#wins").html(wins);
-        $("#wins-losses").html("You won!");
+        // $("#wins-losses").text("You won!");
         resetGame();
     }
 
     else if (totalScore >= targetNumber) {
         losses++;
         $("#losses").html(losses);
-        $("#wins-losses").html("You lost!");
+        // $("#wins-losses").html("You lost!");
         resetGame();
     }
 
 });
+
+
